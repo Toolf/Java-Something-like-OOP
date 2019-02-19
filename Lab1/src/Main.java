5c3c3ef89cb9479d87bab9add3ed0ed83c09480e
Toolf <nazar.smirnov.2001@gmail.com>
2019-02-19 10:05:09 +0200

    Update1

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,17 +1,25 @@
 public class Main {
 	public static void main(String[] args) {
-		float a = 2,n = 5;
-		float b = 6,m = 10;
+		/*
+			C2 = 1
+			C3 = 1
+			C5 = 0
+			C7 = 5
+		*/
+		
+		float a = 2,n = 2;
+		float b = 3,m = 4;
 		final float c = 1;
+		
 		float sum = 0;
 		
-		if((n>=b && a<=b) || (a>=b && a<=m)){
+		if(a <= c && c <= n){
 			System.out.println("Err");
 			return;
 		}
 		for (int i=(int)a;i<=n;i++){
 			for (int j=(int)b;j<=m;j++){
-				sum += (i*j)/(float)(i-j);
+				sum += (float)(i*j)/(i-c);
 			}
 		}
 		System.out.println(sum);
--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -4,7 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		int a = 5,sum=0;
 		char b[] = {1,1,2,2};
+		
 		char c[] = new char[b.length];
+		
 		for(int i =0;i<b.length;i++){
 			c[i] = (char)(b[i]*(char)a);
 			sum+=c[i];
