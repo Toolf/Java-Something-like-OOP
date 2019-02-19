4fed79f196ee9a119b46dec54c0d2aaa56f8859b
Toolf <nazar.smirnov.2001@gmail.com>
2019-02-19 07:49:36 +0200

    optimization

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,24 +1,19 @@
-
 public class Main {
-
 	public static void main(String[] args) {
-		float a = 4,n = 5;
+		float a = 2,n = 5;
 		float b = 6,m = 10;
 		final float c = 1;
 		float sum = 0;
 		
-		for (int i=(int)a;i<n;i++){
-			if(i>=b && i<m){
-				System.out.println("Err");
-				return;
-			}
-			for (int j=(int)b;j<m;j++){
-				System.out.println(sum);
+		if((n>=b && a<=b) || (a>=b && a<=m)){
+			System.out.println("Err");
+			return;
+		}
+		for (int i=(int)a;i<=n;i++){
+			for (int j=(int)b;j<=m;j++){
 				sum += (i*j)/(float)(i-j);
 			}
 		}
 		System.out.println(sum);
 	}
-
-
 }
