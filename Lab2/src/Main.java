e688afb55dcbed190b4b05882401d50a19a85821
Toolf <nazar.smirnov.2001@gmail.com>
2019-02-25 18:06:29 +0200

    update

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -2,24 +2,48 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int a = 5,sum=0;
-		char b[] = {1,1,2,2};
+		//start values (inputs) 
+		int a = 5;
+		char b[][] = {
+				{1,2,3,4},
+				{1,0,0,0},
+				{2,1,1,5},
+				{2,2,2,2}
+		};
 		
-		char c[] = new char[b.length];
-		
-		for(int i =0;i<b.length;i++){
-			c[i] = (char)(b[i]*(char)a);
-			sum+=c[i];
-			System.out.print((int)c[i]);
-			System.out.print(" ");
-		} System.out.print("\n");
-		
-		if(b.length == 0){
-			System.out.println("b is empty");
-		} else {
-			System.out.println((double)sum/b.length);
+		//check b is empty
+		if (b.length == 0 || b[0].length == 0){
+			System.out.println("B is empty");
+			//must return some Exception
+			return;
 		}
 		
+		//change matrix a*B a-const
+		char c[][] = new char[b.length][b[0].length];
+		double partialSums[] = new double[b.length];
+		for(int i=0;i<b.length;i++){
+			for(int j=0;j<b[i].length;j++){
+				c[i][j]= (char)((int)b[i][j]*a);
+				partialSums[i] += c[i][j];
+			}
+			partialSums[i]/=b[i].length;
+		}
+		
+		System.out.print("Finish Matrix:\n");
+		for(int i=0;i<c.length;i++){
+			for(int j=0;j<c[i].length;j++){
+				System.out.print((int)c[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+		
+		double averagValue = 0;
+		for(int i=0;i<partialSums.length;i++){
+			averagValue += partialSums[i];
+		}
+		averagValue/=partialSums.length;
+		System.out.println(averagValue);
+		//must return 8.75
 	}
 
 }
